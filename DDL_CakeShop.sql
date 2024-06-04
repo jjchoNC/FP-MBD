c03604584e0dcb58e39437f1d0842a47faa36ad9
Haalloobim <withbimatara@gmail.com>
2024-06-04 21:36:24 +0700

    feat: add ddl for cake shop

---
+++ DDL_CakeShop.sql
@@ -0,0 +1,119 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2024-06-04 14:34:39.394
+
+-- tables
+-- Table: customer
+CREATE TABLE customer (
+    cst_id char(6)  NOT NULL,
+    cst_name varchar(100)  NOT NULL,
+    cst_phoneNumber varchar(20)  NULL,
+    cst_address varchar(100)  NOT NULL,
+    CONSTRAINT customer_pk PRIMARY KEY (cst_id)
+);
+
+-- Table: employee
+CREATE TABLE employee (
+    emp_id char(6)  NOT NULL,
+    emp_name varchar(100)  NOT NULL,
+    emp_phoneNumber varchar(20)  NULL,
+    emp_address varchar(100)  NOT NULL,
+    emp_jobSection varchar(100)  NOT NULL,
+    CONSTRAINT employee_pk PRIMARY KEY (emp_id)
+);
+
+-- Table: item
+CREATE TABLE item (
+    item_id char(6)  NOT NULL,
+    item_name varchar(100)  NOT NULL,
+    item_price money  NOT NULL,
+    item_category varchar(50)  NOT NULL,
+    CONSTRAINT item_pk PRIMARY KEY (item_id)
+);
+
+-- Table: supplier
+CREATE TABLE supplier (
+    sup_id char(6)  NOT NULL,
+    sup_name varchar(100)  NOT NULL,
+    sup_address varchar(100)  NOT NULL,
+    sup_phone varchar(20)  NULL,
+    CONSTRAINT supplier_pk PRIMARY KEY (sup_id)
+);
+
+-- Table: supplier_item
+CREATE TABLE supplier_item (
+    supplier_sup_id char(6)  NOT NULL,
+    item_item_id char(6)  NOT NULL,
+    item_amount int  NOT NULL,
+    CONSTRAINT supplier_item_pk PRIMARY KEY (supplier_sup_id,item_item_id)
+);
+
+-- Table: transaction
+CREATE TABLE transaction (
+    tr_id char(6)  NOT NULL,
+    tr_totalBill money  NOT NULL,
+    tr_date date  NOT NULL,
+    tr_paymentMethod varchar(50)  NOT NULL,
+    customer_cst_id char(6)  NOT NULL,
+    employee_emp_id char(6)  NOT NULL,
+    CONSTRAINT transaction_pk PRIMARY KEY (tr_id)
+);
+
+-- Table: transaction_item
+CREATE TABLE transaction_item (
+    transaction_tr_id char(6)  NOT NULL,
+    item_item_id char(6)  NOT NULL,
+    item_amount int  NOT NULL,
+    CONSTRAINT transaction_item_pk PRIMARY KEY (transaction_tr_id,item_item_id)
+);
+
+-- foreign keys
+-- Reference: supplier_item_item (table: supplier_item)
+ALTER TABLE supplier_item ADD CONSTRAINT supplier_item_item
+    FOREIGN KEY (item_item_id)
+    REFERENCES item (item_id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: supplier_item_supplier (table: supplier_item)
+ALTER TABLE supplier_item ADD CONSTRAINT supplier_item_supplier
+    FOREIGN KEY (supplier_sup_id)
+    REFERENCES supplier (sup_id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: transaction_customer (table: transaction)
+ALTER TABLE transaction ADD CONSTRAINT transaction_customer
+    FOREIGN KEY (customer_cst_id)
+    REFERENCES customer (cst_id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: transaction_employee (table: transaction)
+ALTER TABLE transaction ADD CONSTRAINT transaction_employee
+    FOREIGN KEY (employee_emp_id)
+    REFERENCES employee (emp_id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: transaction_item_item (table: transaction_item)
+ALTER TABLE transaction_item ADD CONSTRAINT transaction_item_item
+    FOREIGN KEY (item_item_id)
+    REFERENCES item (item_id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: transaction_item_transaction (table: transaction_item)
+ALTER TABLE transaction_item ADD CONSTRAINT transaction_item_transaction
+    FOREIGN KEY (transaction_tr_id)
+    REFERENCES transaction (tr_id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- End of file.
+
