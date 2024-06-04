9fcdefe413a6e0154de0d3fed3497737f14dad64
Haalloobim <withbimatara@gmail.com>
2024-06-04 21:36:53 +0700

    feat: removing cmmt

--- DDL_CakeShop.sql
+++ DDL_CakeShop.sql
@@ -1,8 +1,4 @@
--- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2024-06-04 14:34:39.394
 
--- tables
--- Table: customer
 CREATE TABLE customer (
     cst_id char(6)  NOT NULL,
     cst_name varchar(100)  NOT NULL,
@@ -11,7 +7,6 @@ CREATE TABLE customer (
     CONSTRAINT customer_pk PRIMARY KEY (cst_id)
 );
 
--- Table: employee
 CREATE TABLE employee (
     emp_id char(6)  NOT NULL,
     emp_name varchar(100)  NOT NULL,
@@ -21,7 +16,6 @@ CREATE TABLE employee (
     CONSTRAINT employee_pk PRIMARY KEY (emp_id)
 );
 
--- Table: item
 CREATE TABLE item (
     item_id char(6)  NOT NULL,
     item_name varchar(100)  NOT NULL,
@@ -30,7 +24,6 @@ CREATE TABLE item (
     CONSTRAINT item_pk PRIMARY KEY (item_id)
 );
 
--- Table: supplier
 CREATE TABLE supplier (
     sup_id char(6)  NOT NULL,
     sup_name varchar(100)  NOT NULL,
@@ -39,7 +32,6 @@ CREATE TABLE supplier (
     CONSTRAINT supplier_pk PRIMARY KEY (sup_id)
 );
 
--- Table: supplier_item
 CREATE TABLE supplier_item (
     supplier_sup_id char(6)  NOT NULL,
     item_item_id char(6)  NOT NULL,
@@ -47,7 +39,6 @@ CREATE TABLE supplier_item (
     CONSTRAINT supplier_item_pk PRIMARY KEY (supplier_sup_id,item_item_id)
 );
 
--- Table: transaction
 CREATE TABLE transaction (
     tr_id char(6)  NOT NULL,
     tr_totalBill money  NOT NULL,
@@ -58,7 +49,6 @@ CREATE TABLE transaction (
     CONSTRAINT transaction_pk PRIMARY KEY (tr_id)
 );
 
--- Table: transaction_item
 CREATE TABLE transaction_item (
     transaction_tr_id char(6)  NOT NULL,
     item_item_id char(6)  NOT NULL,
@@ -66,8 +56,6 @@ CREATE TABLE transaction_item (
     CONSTRAINT transaction_item_pk PRIMARY KEY (transaction_tr_id,item_item_id)
 );
 
--- foreign keys
--- Reference: supplier_item_item (table: supplier_item)
 ALTER TABLE supplier_item ADD CONSTRAINT supplier_item_item
     FOREIGN KEY (item_item_id)
     REFERENCES item (item_id)  
@@ -75,7 +63,6 @@ ALTER TABLE supplier_item ADD CONSTRAINT supplier_item_item
     INITIALLY IMMEDIATE
 ;
 
--- Reference: supplier_item_supplier (table: supplier_item)
 ALTER TABLE supplier_item ADD CONSTRAINT supplier_item_supplier
     FOREIGN KEY (supplier_sup_id)
     REFERENCES supplier (sup_id)  
@@ -83,7 +70,6 @@ ALTER TABLE supplier_item ADD CONSTRAINT supplier_item_supplier
     INITIALLY IMMEDIATE
 ;
 
--- Reference: transaction_customer (table: transaction)
 ALTER TABLE transaction ADD CONSTRAINT transaction_customer
     FOREIGN KEY (customer_cst_id)
     REFERENCES customer (cst_id)  
@@ -91,7 +77,6 @@ ALTER TABLE transaction ADD CONSTRAINT transaction_customer
     INITIALLY IMMEDIATE
 ;
 
--- Reference: transaction_employee (table: transaction)
 ALTER TABLE transaction ADD CONSTRAINT transaction_employee
     FOREIGN KEY (employee_emp_id)
     REFERENCES employee (emp_id)  
@@ -99,7 +84,6 @@ ALTER TABLE transaction ADD CONSTRAINT transaction_employee
     INITIALLY IMMEDIATE
 ;
 
--- Reference: transaction_item_item (table: transaction_item)
 ALTER TABLE transaction_item ADD CONSTRAINT transaction_item_item
     FOREIGN KEY (item_item_id)
     REFERENCES item (item_id)  
@@ -107,7 +91,6 @@ ALTER TABLE transaction_item ADD CONSTRAINT transaction_item_item
     INITIALLY IMMEDIATE
 ;
 
--- Reference: transaction_item_transaction (table: transaction_item)
 ALTER TABLE transaction_item ADD CONSTRAINT transaction_item_transaction
     FOREIGN KEY (transaction_tr_id)
     REFERENCES transaction (tr_id)  
@@ -115,5 +98,4 @@ ALTER TABLE transaction_item ADD CONSTRAINT transaction_item_transaction
     INITIALLY IMMEDIATE
 ;
 
--- End of file.
 
