7d4a732024ddcd9aec29634d636923e8fd1e9416
jjchoNC <5025221001@mhs.its.ac.id>
2024-06-05 14:17:37 +0700

    fix: all the ID into 10 characters format

--- DDL_CakeShop.sql
+++ DDL_CakeShop.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE customer (
-    cst_id char(6)  NOT NULL,
+    cst_id char(10)  NOT NULL,
     cst_name varchar(100)  NOT NULL,
     cst_phoneNumber varchar(20)  NULL,
     cst_address varchar(100)  NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE customer (
 );
 
 CREATE TABLE employee (
-    emp_id char(6)  NOT NULL,
+    emp_id char(10)  NOT NULL,
     emp_name varchar(100)  NOT NULL,
     emp_phoneNumber varchar(20)  NULL,
     emp_address varchar(100)  NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE employee (
 );
 
 CREATE TABLE item (
-    item_id char(6)  NOT NULL,
+    item_id char(10)  NOT NULL,
     item_name varchar(100)  NOT NULL,
     item_price money  NOT NULL,
     item_category varchar(50)  NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE item (
 );
 
 CREATE TABLE supplier (
-    sup_id char(6)  NOT NULL,
+    sup_id char(10)  NOT NULL,
     sup_name varchar(100)  NOT NULL,
     sup_address varchar(100)  NOT NULL,
     sup_phone varchar(20)  NULL,
@@ -34,25 +34,25 @@ CREATE TABLE supplier (
 );
 
 CREATE TABLE supplier_item (
-    supplier_sup_id char(6)  NOT NULL,
-    item_item_id char(6)  NOT NULL,
+    supplier_sup_id char(10)  NOT NULL,
+    item_item_id char(10)  NOT NULL,
     item_amount int  NOT NULL,
     CONSTRAINT supplier_item_pk PRIMARY KEY (supplier_sup_id,item_item_id)
 );
 
 CREATE TABLE transaction (
-    tr_id char(6)  NOT NULL,
+    tr_id char(10)  NOT NULL,
     tr_totalBill money  NOT NULL,
     tr_date date  NOT NULL,
     tr_paymentMethod varchar(50)  NOT NULL,
-    customer_cst_id char(6)  NOT NULL,
-    employee_emp_id char(6)  NOT NULL,
+    customer_cst_id char(10)  NOT NULL,
+    employee_emp_id char(10)  NOT NULL,
     CONSTRAINT transaction_pk PRIMARY KEY (tr_id)
 );
 
 CREATE TABLE transaction_item (
-    transaction_tr_id char(6)  NOT NULL,
-    item_item_id char(6)  NOT NULL,
+    transaction_tr_id char(10)  NOT NULL,
+    item_item_id char(10)  NOT NULL,
     item_amount int  NOT NULL,
     CONSTRAINT transaction_item_pk PRIMARY KEY (transaction_tr_id,item_item_id)
 );
--- QueryLiat_CakeShop.sql
+++ QueryLiat_CakeShop.sql
@@ -20,7 +20,7 @@ CREATE OR REPLACE FUNCTION validateStock()
 RETURNS TRIGGER AS $$
 BEGIN
     IF (NEW.item_amount > (SELECT item_stock FROM item WHERE item_id = NEW.item_item_id)) THEN
-        RAISE EXCEPTION 'Not enough stock available for item %', NEW.item_item_id;
+        RAISE EXCEPTION 'Stok tidak mencukupi untuk item %', NEW.item_item_id;
     END IF;
     RETURN NEW;
 END;
