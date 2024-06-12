f1c825ae96aa172fb862f08c6254fd95b91e7dc3
Haalloobim <withbimatara@gmail.com>
2024-06-12 12:27:39 +0700

    feat: move to new dml files

---
+++ DML_Customer.sql
@@ -0,0 +1,4 @@
+INSERT INTO customer (cst_id, cst_name, cst_phoneNumber, cst_address) VALUES
+('CST0000001', 'John Doe', '1234567890', '123 Main St'),
+('CST0000002', 'Jane Smith', '2345678901', '456 Oak St'),
+('CST0000003', 'Alice Johnson', '3456789012', '789 Pine St');
