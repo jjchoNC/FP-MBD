6b9cffb856bd25eab46ed4ce158cee1d1f753e96
jjchoNC <5025221001@mhs.its.ac.id>
2024-06-12 13:32:00 +0700

    feat: update payment method on transaction

--- DML_Transaction.sql
+++ DML_Transaction.sql
@@ -1,4 +1,4 @@
 INSERT INTO transaction (tr_id, tr_totalBill, tr_date, tr_paymentMethod, customer_cst_id, employee_emp_id) VALUES
-('TRX0000001', 370000, '2023-01-01', 'Credit Card', 'CST0000001', 'EMP0000002'),
-('TRX0000002', 175000, '2023-01-02', 'Cash', 'CST0000002', 'EMP0000002'),
-('TRX0000003', 175000, '2023-01-03', 'Debit Card', 'CST0000003', 'EMP0000002');
+('TRX0000001', 370000, '2023-01-01', 'credit', 'CST0000001', 'EMP0000002'),
+('TRX0000002', 175000, '2023-01-02', 'cash', 'CST0000002', 'EMP0000002'),
+('TRX0000003', 175000, '2023-01-03', 'QRIS', 'CST0000003', 'EMP0000002');
