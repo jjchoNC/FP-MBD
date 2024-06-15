d5a650f0545d122042b9da308366bb525b6e242f
jjchoNC <5025221001@mhs.its.ac.id>
2024-06-15 20:00:56 +0700

    feat: add employee dml

---
+++ DML_Employee.sql
@@ -0,0 +1,100 @@
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000001', 'Jeffery Terry', '9959085639', '484 Rhonda Canyon Suite 700
+Lake Tonya, FL 63961', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000002', 'Matthew Reyes', '0068270435', '0827 Derrick Tunnel
+New Kevin, PW 58103', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000003', 'Aaron Camacho', '0125483171', '26023 Johnson Stream Suite 446
+Williamton, DC 09618', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000004', 'David Gray', '8993372726', '879 Vasquez Forge
+Alvarezburgh, IA 44618', 'Manager');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000005', 'Julian Wilson', '3837607018', '35967 Erika Villages
+Kathyview, AK 89631', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000006', 'Emily Allen', '8762175171', '8834 Justin Mall Apt. 172
+New Hannahborough, SD 46827', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000007', 'Joseph Allen', '1604982069', '3557 Stephanie Unions
+New Christophermouth, NC 74040', 'Cashier');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000008', 'Brittany Sanders', '3968581894', '888 Moreno Way
+Ronaldburgh, SD 47285', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000009', 'Riley Ramirez', '6947956118', 'PSC 8824, Box 1658
+APO AA 11825', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000010', 'Kristen Thomas', '8861159210', '4102 Virginia Spur Suite 187
+Jacobsport, WY 45638', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000011', 'Mrs. Cheryl Sanders', '3454720964', '0376 Chavez Knolls Apt. 608
+West Brandonstad, AR 89962', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000012', 'Jason Rodriguez', '6547236379', 'Unit 8531 Box 0260
+DPO AA 35512', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000013', 'Vanessa Williams', '2786459024', '743 Joy Divide Apt. 497
+Alexandershire, AZ 30521', 'Cashier');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000014', 'Chad Cain', '0070934458', '01926 Schultz Ford
+Hernandeztown, IL 25872', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000015', 'Felicia Lucas', '9771798831', '54638 Davis Meadows
+West Derrick, GA 71568', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000016', 'Danny Rivas', '1616035869', '102 Amanda Mews Suite 336
+South Alicia, KS 38593', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000017', 'Margaret Rodriguez', '0536886326', '65578 Edward Passage Apt. 133
+New Cynthia, IN 66479', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000018', 'Sherri Gomez', '9285599400', '7064 Wright Summit Suite 873
+Woodsville, CO 51085', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000019', 'Dr. Thomas Obrien', '2710669215', '1586 Samuel Course
+West Wendyborough, IN 54633', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000020', 'Jaime Spencer', '2027898843', '2529 Lisa River
+North Barbara, PW 82846', 'Cashier');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000021', 'Parker Price', '1076747606', '8997 Tyrone Circle Suite 911
+Michelletown, MO 74779', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000022', 'Kelly French', '1857071600', 'Unit 3704 Box 5927
+DPO AE 26194', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000023', 'Taylor Lane', '7448779302', 'Unit 6224 Box 3547
+DPO AE 87748', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000024', 'Megan Smith', '0261346933', '87718 Scott Lock Suite 108
+Lewischester, GU 54150', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000025', 'Ashley Rodgers', '8863533428', '692 Sandoval Cove
+Mcdonaldton, KS 39540', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000026', 'Colin Hughes', '4789278795', '5412 Hall Glen Apt. 609
+Nancytown, KS 71325', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000027', 'Robert Hill', '9341151668', '677 Stephen Drives
+North Joshuaside, GU 05555', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000028', 'Alicia Brooks', '3892873802', '728 Rhonda Manor
+Barkerborough, AS 08123', 'Cashier');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000029', 'Chad Palmer', '1493333354', '70119 Garrison Via Suite 912
+South Annfurt, KY 62759', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000030', 'Kimberly Murphy', '2033162633', '9128 Amber Union Apt. 299
+Patriciafort, KY 82440', 'Cashier');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000031', 'Robert Maldonado', '3581506022', '755 Vanessa Cape Suite 619
+New Amandaberg, IN 06363', 'Cashier');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000032', 'William French', '0672997031', '9580 Kim Camp Suite 100
+Lake Crystal, MA 62959', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000033', 'Claudia Bowen', '3254673167', '777 Skinner Cliffs Apt. 196
+North Alexandria, DE 23316', 'Cashier');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000034', 'Daniel Underwood', '3439522970', '294 Wendy Centers
+Wardton, MD 75061', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000035', 'Michelle Wallace', '8179810300', 'Unit 4520 Box 7676
+DPO AP 73099', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000036', 'Pamela Torres', '8159084339', '3419 Regina Island
+North Jennifershire, PW 39499', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000037', 'Jodi Miller', '1422854175', '767 Melissa Fork
+South Briannaberg, SC 10715', 'Cashier');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000038', 'Kelsey Robinson', '9452569483', 'USNV Wilson
+FPO AE 36048', 'Cashier');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000039', 'Benjamin Anderson', '3988187899', '3140 Walters Corners Apt. 465
+Anthonymouth, ME 25770', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000040', 'Eric Vazquez', '5586277461', '915 Mary Square Apt. 277
+New Tommytown, OH 05309', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000041', 'Pamela Nguyen', '5018561449', '424 Daniel Corners Suite 413
+East Brian, IL 61012', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000042', 'William Young', '0766652185', '625 Coleman Forges
+Lake Tinafurt, NE 39688', 'Admin');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000043', 'Deborah Roy', '5700820204', '74856 Lloyd Roads
+Lake Jenniferberg, TX 27476', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000044', 'Larry Howard', '1192519917', 'Unit 2088 Box 6125
+DPO AA 91004', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000045', 'Rachel Crosby', '8994754919', '706 Alexis Trace
+Erinfurt, NJ 99549', 'Cashier');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000046', 'Randy Oliver', '9018384399', '12096 Fisher Points Apt. 363
+North Rita, ME 83518', 'Cashier');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000047', 'Brandy Bryant', '3603040914', '12004 Velasquez Fork
+South Jacquelinechester, MH 60683', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000048', 'Paul Molina', '4293166559', '54573 Rice Garden
+North Connieborough, VI 80994', 'Warehouse');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000049', 'Stephen Waters', '9967356163', '20884 Andrea Lodge
+Griffinbury, MH 12717', 'Cashier');
+INSERT INTO employee (emp_id, emp_name, emp_phoneNumber, emp_address, emp_jobSection) VALUES ('EMP0000050', 'Dr. Joel Winters', '0276638463', '13232 Flores Inlet
+North Curtis, CO 94964', 'Warehouse');
