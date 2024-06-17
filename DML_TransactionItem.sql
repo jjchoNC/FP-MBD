f8bc14bdd845ce9ae6d0d8880b032268d2057e9d
jjchoNC <5025221001@mhs.its.ac.id>
2024-06-17 20:44:42 +0700

    feat: update dummy data on transaction

--- DML_TransactionItem.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO transaction_item (transaction_tr_id, item_item_id, item_amount) VALUES
-('TRX0000001', 'ITM0000001', 2),
-('TRX0000001', 'ITM0000002', 3),
-('TRX0000002', 'ITM0000003', 1),
-('TRX0000003', 'ITM0000003', 1);
